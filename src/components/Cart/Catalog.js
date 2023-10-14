33313335b708b08daccf5b2c6019e12235de6c13
Chavalam <varshi1188@gmail.com>
2023-10-14 16:18:14 +0530

    Update product image size and add product details table in modal

--- src/components/Cart/Catalog.js
+++ src/components/Cart/Catalog.js
@@ -18,7 +18,7 @@ export default function Catalog() {
         product_sub_category: "appliance",
         product_title: "LG PS-Q19MNZF 1.5 Ton 5 Star",
     }
-      )
+    )
 
     // Use the useEffect hook to fetch data when the 'search' state changes
     useEffect(() => {
@@ -31,6 +31,7 @@ export default function Catalog() {
             .then((res) => {
                 setData(res);
                 setLoading(false); // Hide loading message after data is fetched
+                console.log(data.data)
             })
             .catch((error) => {
                 console.error(error);
@@ -61,7 +62,10 @@ export default function Catalog() {
                         <div key={index} class="col-md-3 py-3 py-md-0">
                             {/* {console.log(item)} */}
                             <div class="card">
-                                <img src={item.product_image} alt="" />
+                                <img src={item.product_image} alt="" style={{
+                                    width: '100%', // Set the width to 100% to ensure images fill the container
+                                    height: '200px' // Set a fixed height for all images (adjust as needed)
+                                }} />
                                 <div class="card-body">
                                     <h3 class="text-center">{item.product_title}</h3>
                                     <p class="text-center">Category : {item.product_category}</p>
@@ -72,8 +76,8 @@ export default function Catalog() {
                                         <i class="fa-solid fa-star checked"></i>
                                         <i class="fa-solid fa-star checked"></i>
                                     </div>
-                                    <h2> &#x20b9;{item.product_lowest_price} <span><li class="fa-solid fa-cart-shopping"></li></span></h2>
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={()=>{
+                                    <h2> &#x20b9;{item.product_lowest_price}</h2>
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => {
                                         setModal(item)
                                     }}>
                                         Know More
@@ -84,16 +88,36 @@ export default function Catalog() {
                         </div>
                     ))}
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-dialog modal-lg modal-dialog-centered">
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h5 class="modal-title" id="exampleModalLabel">{modal.product_title}</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body ">
-                                    <img src={modal.product_image}/>
-                                    <p class="text-center">Category : {modal.product_category}</p>
-                                    {/* <h2> &#x20b9;{modal.product_lowest_price} <span><li class="fa-solid fa-cart-shopping"></li></span></h2> */}
+                                    <div className='d-flex justify-content-center align-items-center'>
+                                        <img src={modal.product_image}  />
+                                    </div>
+                                    <table className="table table-bordered table-striped table-hover">
+                                        <thead className='bg-info text-white'>
+                                            <tr >
+                                                <th>ID</th>
+                                                <th>Title</th>
+                                                <th>Category</th>
+                                                <th>Price (&#x20b9;)</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr key={modal.product_id}>
+                                                <td>{modal.product_id}</td>
+                                                <td>{modal.product_title}</td>
+                                                <td>{modal.product_category}</td>
+                                                <td>{modal.product_lowest_price}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    {/* <p>Category : {modal.product_category}</p>
+                                    <p>price :  &#x20b9;{modal.product_lowest_price}</p> */}
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
